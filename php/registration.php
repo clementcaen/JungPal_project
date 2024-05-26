df8176c46e8996b0ad6fa28863b44e4b091829bb
Marylinelb2105 <marylinelebot21@gmail.com>
2024-05-26 13:05:39 +0200

    V1.2.3 inscription backend

--- php/registration.php
+++ php/registration.php
@@ -12,23 +12,26 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 include("bdd.php");
+
 $mail = $pdo->prepare("select * from users where email='$email' limit 1");
 $mail->execute();
 $result = $mail->fetchAll();
+
 if (count($result) > 0){
     $resultat = true;
-}
-else {  //si mail différent, insérer dans la bdd
+} else {
     $resultat = false;
     $insert = $pdo->prepare("insert into users(name, surname, gender, dob, address, city, postal_code, email, password) values(?,?,?,?,?,?,?,?,?)");
     $insert->execute(array($name, $surname, $gender, $dob, $address, $city, $postal_code, $email, $password));
     
-    if ($conn->query($insert) === TRUE) {
-        echo "New record created successfully";
+    if ($insert) {
+        header("Location: ../html/connexion.html");
+        exit(); // Assurez-vous d'ajouter exit après la redirection pour arrêter l'exécution du script
     } else {
-        echo "Error: " . $insert . "<br>" . $conn->error;
+        echo "Error: " . $insert . "<br>" . $pdo->error;
     }
-  }
+}
 
-  echo json_encode($resultat);
+echo json_encode($resultat);
+
 ?>
