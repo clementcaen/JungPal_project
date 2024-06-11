f9c1148535c0abe974ff5937a9b0a26e66e25d89
maxime <maxime.cayman@isen-ouest.yncrea.fr>
2024-06-11 13:31:39 +0200

    Update code

---
+++ js/login.js
@@ -0,0 +1,30 @@
+document.getElementById("login").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    let formData = new FormData(document.querySelector("form"));
+
+    ajaxRequest("POST", "../php/login.php", formData, function(response) {
+        if (response.success) {
+            window.location.href = "../html/homepage.html";
+            alert(response.message);
+        } else {
+            alert("Erreur: " + response.message); // Afficher le message d'erreur
+        }
+    });
+});
+
+function ajaxRequest(method, url, data, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            callback(JSON.parse(xhr.responseText));
+        } else {
+            alert("Erreur de requête: " + xhr.status); // Afficher le code d'erreur HTTP
+        }
+    };
+    xhr.onerror = function() {
+        alert("Erreur de connexion au serveur."); // Afficher une erreur en cas d'échec de la requête
+    };
+    xhr.send(data);
+}
---
+++ php/login.php
@@ -0,0 +1,38 @@
+<?php
+// Connexion à la base de données (assurez-vous d'adapter ces informations à votre configuration)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "elderly";
+
+// Création de la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
+}
+
+// Récupération des données du formulaire
+$email = $_POST['email'];
+$mot_de_passe = $_POST['password'];
+
+// Requête SQL pour vérifier les informations de connexion
+$sql = "SELECT * FROM users WHERE email='$email' AND password='$mot_de_passe'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // L'utilisateur est authentifié avec succès
+    $row = $result->fetch_assoc();
+    $nom_utilisateur = $row['first_name'];
+    $response = array("success" => true, "message" => "Bienvenue, " . $nom_utilisateur);
+} else {
+    // Les informations de connexion sont incorrectes
+    $response = array("success" => false, "message" => "Identifiants incorrects. Veuillez réessayer.");
+}
+
+// Fermer la connexion à la base de données
+$conn->close();
+
+echo json_encode($response);
+?>
