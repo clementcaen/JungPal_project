ce97d7028f52a64b491b1a836cd187075ce4b8e2
maxime <maxime.cayman@isen-ouest.yncrea.fr>
2024-06-20 20:01:46 +0200

    Amélioration du CSS pour une mise en page plus clair

---
+++ js/scroll.js
@@ -0,0 +1,4 @@
+document.getElementById('about').addEventListener('click', function() {
+    // Redirection vers le paragraphe avec l'ID "targetParagraph"
+    document.getElementById('text_chatbox').scrollIntoView({ behavior: 'smooth' });
+});
