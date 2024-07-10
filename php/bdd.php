acafbb621fff88eec1ed505b9da26c0e7e00908e
maxime <maxime.cayman@isen-ouest.yncrea.fr>
2024-07-10 20:23:07 +0200

    Update bdd.php

--- php/bdd.php
+++ php/bdd.php
@@ -1,14 +1,23 @@
 <?php
-$serverName = getenv('DB_SERVER');
-$database = getenv('DB_NAME');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
+ini_set('display_errors','on');
+error_reporting(E_ALL);
 
-try {
-    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie!";
-} catch (PDOException $e) {
-    echo "Erreur de connexion: " . $e->getMessage();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Connexion à la base de données (assurez-vous d'adapter ces informations à votre configuration)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jungpal";
+
+// Création de la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
 }
+
 ?>
