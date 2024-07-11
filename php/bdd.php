1a018dc76a13e296676117ffa22e0d8a72bc5239
clément Jaminion <87208881+clementcaen@users.noreply.github.com>
2024-07-11 14:14:19 +0200

    Update bdd.php

--- php/bdd.php
+++ php/bdd.php
@@ -6,32 +6,18 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-// // Connexion à la base de données (assurez-vous d'adapter ces informations à votre configuration)
-// $servername = "tcp:jung-project.database.windows.net,1433";
-// $username = "jung-project";
-// $password = "19e82fe8-2cd2-47a0-917c-cd16ebf5b389";
-// $dbname = "jung-project";
+// Connexion à la base de données (assurez-vous d'adapter ces informations à votre configuration)
+$servername = "jungpal.mysql.database.azure.com";
+$username = "vaipedxqaa";
+$password = "2T5$9R8$cQSCrL7m";
+$dbname = "jungpal-database";
 
-// // Création de la connexion
-// $conn = new mysqli($servername, $username, $password, $dbname);
+// Création de la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// // Vérifier la connexion
-// if ($conn->connect_error) {
-//     die("La connexion a échoué : " . $conn->connect_error);
-// }
-    
-try {
-    $conn = new PDO("sqlsrv:server = tcp:jung-project.database.windows.net,1433; Database = jung-project", "jung-project", "19e82fe8-2cd2-47a0-917c-cd16ebf5b389");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "jung-project", "pwd" => "19e82fe8-2cd2-47a0-917c-cd16ebf5b389", "Database" => "jung-project", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:jung-project.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 ?>
